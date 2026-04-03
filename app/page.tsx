dfaf115f99e59b2328e8ce05944b3b00b536516a
rifat-hasan <rifathasannetrakona@gmail.com>
2026-04-03 21:58:45 +0600

    feat: initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,20 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen bg-white">
-
+      <main className="flex-1">
+        {/* hero section */}
+        <section className="container mx-auto px-4 py-12">
+          <div className="mx-auto max-w-4xl px-4 py-32">
+            <h1 className="text-4xl font-bold">Job Tracker</h1>
+            <p className="text-lg">Track your job applications</p>
+          
+          <div>
+            <button>Start for Free</button>
+            <p>Free forever, no creadit card requared</p>
+          </div>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
